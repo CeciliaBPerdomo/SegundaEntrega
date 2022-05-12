4a11595750bada8b0e855e6931a88638df50ebc5
Cecilia <cecilia.perdomo@gmail.com>
2022-05-11 21:07:07 -0300

    commit 4

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ npm i mongoose
 npm i firebase-admin
 npm i express
 */
-import express  from "express";
+import express from "express";
 const { Router } = express
 
 import {
@@ -15,3 +15,53 @@ import {
 const app = express()
 
 const esAdmin = true
+
+//47.20
+
+function noEsAdmin(ruta, metodo){
+    const error = {
+        error: -1
+    }
+    if(ruta && metodo){
+        error.descripcion = `ruta '${ruta}' metodo '${metodo}' no autorizado`
+    } else {
+        error.descripcion = 'no autorizado'
+    }
+    return error
+}
+
+function soloAdmin(req, res, next){
+    if(!esAdmin){
+        res.JSON(noEsAdmin())
+    }else{
+        next()
+    }
+}
+
+/* Productos */
+const productosRouter = new Router()
+
+productosRouter.get('/', async (req, res) => {
+    const productos = await productoApi.listarTodos()
+    res.JSON(productos)
+})
+
+productosRouter.get('/:id', async(req, res) => {
+    res.JSON(await productoApi.listar(req.params.id))
+})
+
+productosRouter.post('/', async(req, res) => {
+    res.JSON(await productoApi.guardar(req.body))
+})
+
+productosRouter.put('/:id', async(req, res) => {
+    res.JSON(await productoApi.actualizar(req.body))
+})
+
+productosRouter.delete('/:id', async(req, res) => {
+    res.JSON(await productoApi.borrar(req.params.id))  
+})
+
+productosRouter.delete('/', async(req, res) => {
+    res.JSON(await productoApi.borrarTodos())
+})
